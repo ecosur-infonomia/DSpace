21a3fb383055f6ff656d91113d10e39131291563
Andrew Waterman <andrew.waterman@gmail.com>
2013-05-23 15:31:07 -0500

    Commit of Richard Jones updates to Sword2 packages.

--- dspace-swordv2/src/main/java/org/dspace/sword2/SimpleDCMetadata.java
+++ dspace-swordv2/src/main/java/org/dspace/sword2/SimpleDCMetadata.java
@@ -1,3 +1,12 @@
+/**
+ * <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+ * <html><head>
+ * <title>301 Moved Permanently</title>
+ * </head><body>
+ * <h1>Moved Permanently</h1>
+ * <p>The document has moved <a href="https://svn.duraspace.org/dspace/licenses/LICENSE_HEADER">here</a>.</p>
+ * </body></html>
+ */
 package org.dspace.sword2;
 
 import java.util.HashMap;
--- dspace-swordv2/src/main/java/org/dspace/sword2/SwordConfigurationDSpace.java
+++ dspace-swordv2/src/main/java/org/dspace/sword2/SwordConfigurationDSpace.java
@@ -1,9 +1,11 @@
 /**
- * The contents of this file are subject to the license and copyright
- * detailed in the LICENSE and NOTICE files at the root of the source
- * tree and available online at
- *
- * http://www.dspace.org/license/
+ * <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+ * <html><head>
+ * <title>301 Moved Permanently</title>
+ * </head><body>
+ * <h1>Moved Permanently</h1>
+ * <p>The document has moved <a href="https://svn.duraspace.org/dspace/licenses/LICENSE_HEADER">here</a>.</p>
+ * </body></html>
  */
 package org.dspace.sword2;
 
@@ -215,6 +217,11 @@ public class SwordConfigurationDSpace implements SwordConfiguration
     public String getAlternateUrlContentType()
     {
         return ConfigurationManager.getProperty("swordv2-server", "error.alternate.content-type");
+    }
+
+    @Override
+    public boolean allowUnauthenticatedMediaAccess() {
+        return true;
     }
 
     ///////////////////////////////////////////////////////////////////////////////////
